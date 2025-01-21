097c59ad93b9ba657c45d8ff3830d6feeda53431
hbasrik <basri.kocagol@ug.bilkent.edu.tr>
2025-01-22 01:38:43 +0300

    Finalize the no-db confşigs

--- config/auth.php
+++ config/auth.php
@@ -61,7 +61,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'array',
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
 
