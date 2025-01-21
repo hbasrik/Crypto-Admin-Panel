e23ffcaadc4992e7996e025ef94222c676fe7f3d
hbasrik <basri.kocagol@ug.bilkent.edu.tr>
2025-01-21 03:25:21 +0300

    CSS folder is created and tested

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -4,6 +4,7 @@
 <head>
     <title>Crypto Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
