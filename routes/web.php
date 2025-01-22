38166e476af191fa493a39ef16fd048f9872f27d
hbasrik <basri.kocagol@ug.bilkent.edu.tr>
2025-01-22 18:26:29 +0300

    Setting up the database configs, lıogin-register pages are created

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CryptoController;
+use Illuminate\Support\Facades\Auth;
 
+// Authentication routes
+Auth::routes();
 
-Route::get('/', [CryptoController::class, 'index'])->name('crypto.index');
-Route::get('/crypto/chart-data/{id}', [CryptoController::class, 'getChartData'])->name('crypto.chart-data');
+// Redirect to login if not authenticated
+Route::get('/', function () {
+    return redirect('login');
+});
+
+// Routes protected by 'auth' middleware
+Route::middleware(['auth'])->group(function () {
+    // Dashboard route
+    Route::get('/dashboard', [CryptoController::class, 'index'])->name('dashboard');
+
+    // Redirect '/home' to '/dashboard'
+    Route::get('/home', function () {
+        return redirect('/dashboard');
+    });
+
+    // Crypto routes
+    Route::get('/crypto', [CryptoController::class, 'index'])->name('crypto.index');
+    Route::get('/crypto/chart-data/{id}', [CryptoController::class, 'getChartData'])->name('crypto.chart-data');
+});
